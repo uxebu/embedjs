cf28e006357d0e0879db4f885ed8e8c0298d102b
Tobias von Klipstein <tobias@uxebu.com>
2010-04-14 21:41:10 +0200

    added a much simpler build file

--- build/dojo.build.js
+++ build/dojo.build.js
@@ -1,36 +1,10 @@
 ({
     baseUrl: "./src",
-    dir: "./src-build",
-    requireUrl: "./src/require.js",
-    // we need to execute the modules
-    // to catch the require.modify / require.alter calls
     execModules: true,
-    /*alternatives:{
-        "dojo/array":"dojo/array/functional"
-    },
-    modifiers:{
-        "dojo/array":"dojo/array/functional_addon"
-    },*/
-    pragmas: {
-        requireExcludePlugin: true,
-        requireExcludePageLoad: true,
-        requireExcludeContext: true
-    },
-    modules: [
-        {
-            name: "build/dojo-windows-mobile-v6.5-widget",
-            include: [
-                "_platforms/windows-mobile-v6.5-widget",
-                "_features/dojo-kitchen-sink"
-            ]
-        },
-        {
-            // TODO: ignoring depedenceny on the first module!
-            name: "build/dojo-android-phonegap",
-            include: [
-                "_platforms/android-phonegap",
-                "_features/dojo-kitchen-sink"
-            ]
-        }
+    includeRequire: true,
+    out: "dist/dojo-windows-mobile-v6.5-widget.js",
+    include: [
+        "dojo/_platforms/windows-mobile-v6.5-widget",
+        "dojo/_features/dojo-kitchen-sink"
     ]
 })
