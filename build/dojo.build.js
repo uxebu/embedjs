994a45b237285020876aa115ed1fc1930ae40e98
Tobias von Klipstein <tobias@uxebu.com>
2010-04-25 20:24:29 +0200

    added an optional build param

--- build/dojo.build.js
+++ build/dojo.build.js
@@ -1,10 +1,11 @@
-({
+{
     baseUrl: "./src",
     execModules: true,
     includeRequire: true,
+    //skipModuleInsertion: true,
     out: "dist/dojo-windows-mobile-v6.5-widget.js",
     include: [
         "dojo/_platforms/windows-mobile-v6.5-widget",
         "dojo/_features/dojo-kitchen-sink"
     ]
-})
+}
