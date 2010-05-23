fcbdc4be0214a5695b6d649b91f907a0f7477111
Tobias von Klipstein <tobias@uxebu.com>
2010-05-23 20:32:48 +0200

    added more modules to build profile

--- src/dojo/_features/dojo-kitchen-sink.js
+++ src/dojo/_features/dojo-kitchen-sink.js
@@ -1,5 +1,8 @@
 require.def("dojo_kitchen_sink", [
     "dojo",
     "dojo/oo/declare",
-    "dojo/array"
+    "dojo/array",
+    "dojo/lang/clone",
+    "dojo/lang/hitch",
+    "dojo/lang/string"
 ]);
