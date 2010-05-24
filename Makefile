345f13ff12a5e17fe568cbdcfd5964d06a715a44
Tobias von Klipstein <tobias@uxebu.com>
2010-05-24 17:36:07 +0200

    Added new build profiles to Makefile, removed old dojo.build.js, added latest changes to dojo.js

--- Makefile
+++ Makefile
@@ -14,7 +14,13 @@ BUILD_PROFILE_DIR := $(ROOT_DIR)/build
 .PHONY: clean
 
 dojo-build:
-	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo.build.js
+	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo-common.build.js
+	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo-webkit-mobile.build.js
+	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo-windows-mobile.build.js
+	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo-nokia-wrt.build.js
+	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo-opera.build.js
+	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo-blackberry.build.js
+	$(BUILD_TOOL) $(BUILD_PROFILE_DIR)/dojo-blackberry46.build.js
 
 clean:
 	rm -rf $(DIST_DIR)
--- src/dojo.js
+++ src/dojo.js
@@ -3,16 +3,19 @@ var djConfig = dojo.config = {};
 
 require.def("dojo", function(){
 
-//dojo = {};
-
-//dojo.global = window;
+dojo.global = window;
 
 dojo.doc = document;
 dojo.body = function() {
 	return document.body;
 }
 
-dojo.provide = function(){};
+dojo.provide = function(resourceName){
+	// Make sure we have a string.
+	resourceName = resourceName + "";
+	return dojo.getObject(resourceName, true); // Object
+};
+
 dojo.require = function(){};
 
 (function(d){
@@ -212,5 +215,146 @@ dojo.require = function(){};
 			d._callLoaded();
 		}
 	}
+	
+	dojo._getModuleSymbols = function(/*String*/modulename){
+		// summary:
+		//		Converts a module name in dotted JS notation to an array
+		//		representing the path in the source tree
+		var syms = modulename.split(".");
+		for(var i = syms.length; i>0; i--){
+			var parentModule = syms.slice(0, i).join(".");
+			if(i == 1 && !d._moduleHasPrefix(parentModule)){
+				// Support default module directory (sibling of dojo) for top-level modules
+				syms[0] = "../" + syms[0];
+			}else{
+				var parentModulePath = d._getModulePrefix(parentModule);
+				if(parentModulePath != parentModule){
+					syms.splice(0, i, parentModulePath);
+					break;
+				}
+			}
+		}
+		return syms; // Array
+	};
+
+	dojo.moduleUrl = function(/*String*/module, /*dojo._Url||String*/url){
+		//	summary:
+		//		Returns an URL relative to a module.
+		//	example:
+		//	|	var pngPath = dojo.moduleUrl("acme","images/small.png");
+		//	|	console.dir(pngPath); // list the object properties
+		//	|	// create an image and set it's source to pngPath's value:
+		//	|	var img = document.createElement("img");
+		// 	|	// NOTE: we assign the string representation of the url object
+		//	|	img.src = pngPath.toString();
+		//	|	// add our image to the document
+		//	|	dojo.body().appendChild(img);
+		//	example:
+		//		you may de-reference as far as you like down the package
+		//		hierarchy.  This is sometimes handy to avoid lenghty relative
+		//		urls or for building portable sub-packages. In this example,
+		//		the `acme.widget` and `acme.util` directories may be located
+		//		under different roots (see `dojo.registerModulePath`) but the
+		//		the modules which reference them can be unaware of their
+		//		relative locations on the filesystem:
+		//	|	// somewhere in a configuration block
+		//	|	dojo.registerModulePath("acme.widget", "../../acme/widget");
+		//	|	dojo.registerModulePath("acme.util", "../../util");
+		//	|
+		//	|	// ...
+		//	|
+		//	|	// code in a module using acme resources
+		//	|	var tmpltPath = dojo.moduleUrl("acme.widget","templates/template.html");
+		//	|	var dataPath = dojo.moduleUrl("acme.util","resources/data.json");
+
+		var loc = d._getModuleSymbols(module).join('/');
+		if(!loc){ return null; }
+		if(loc.lastIndexOf("/") != loc.length-1){
+			loc += "/";
+		}
+
+		//If the path is an absolute path (starts with a / or is on another
+		//domain/xdomain) then don't add the baseUrl.
+		var colonIndex = loc.indexOf(":");
+		if(loc.charAt(0) != "/" && (colonIndex == -1 || colonIndex > loc.indexOf("/"))){
+			loc = d.baseUrl + loc;
+		}
+
+		return loc + (url || "");
+	};
+
+
+	d.mixin(d, {
+		baseUrl: "",
+		_loadedModules: {},
+		_inFlightCount: 0,
+		_hasResource: {},
+
+		_modulePrefixes: {
+			dojo: 	{	name: "dojo", value: "." },
+			// dojox: 	{	name: "dojox", value: "../dojox" },
+			// dijit: 	{	name: "dijit", value: "../dijit" },
+			doh: 	{	name: "doh", value: "../util/doh" },
+			tests: 	{	name: "tests", value: "tests" }
+		},
+
+		_moduleHasPrefix: function(/*String*/module){
+			// summary: checks to see if module has been established
+			var mp = d._modulePrefixes;
+			return !!(mp[module] && mp[module].value); // Boolean
+		},
+
+		_getModulePrefix: function(/*String*/module){
+			// summary: gets the prefix associated with module
+			var mp = d._modulePrefixes;
+			if(d._moduleHasPrefix(module)){
+				return mp[module].value; // String
+			}
+			return module; // String
+		},
+
+		_loadedUrls: [],
+
+		//WARNING:
+		//		This variable is referenced by packages outside of bootstrap:
+		//		FloatingPane.js and undo/browser.js
+		_postLoad: false,
+
+		//Egad! Lots of test files push on this directly instead of using dojo.addOnLoad.
+		_loaders: [],
+		_unloaders: [],
+		_loadNotifying: false
+	});
 })(dojo);
+
+(function(d){
+
+	// grab the node we were loaded from
+	if(document && document.getElementsByTagName){
+		var scripts = document.getElementsByTagName("script");
+		var rePkg = /dojo[^\/]*\.js(\W|$)/i;
+		for(var i = 0; i < scripts.length; i++){
+			var src = scripts[i].getAttribute("src");
+			if(!src){ continue; }
+			var m = src.match(rePkg);
+			if(m){
+				// find out where we came from
+				if(!d.config.baseUrl){
+					d.config.baseUrl = src.substring(0, m.index);
+				}
+				// and find out if we need to modify our behavior
+				var cfg = scripts[i].getAttribute("djConfig");
+				if(cfg){
+					var cfgo = eval("({ "+cfg+" })");
+					for(var x in cfgo){
+						dojo.config[x] = cfgo[x];
+					}
+				}
+				break; // "first Dojo wins"
+			}
+		}
+	}
+	d.baseUrl = d.config.baseUrl;
+}(dojo));
+
 });
