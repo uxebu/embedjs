7fc7166dfea2183c77c47ed86015329b1dacc203
Wolfram Kriesing <wk@uxebu.com>
2010-08-01 13:40:44 +0200

    First version of plain building.

---
+++ build/kitchensink/webkit-mobile.sh
@@ -0,0 +1,9 @@
+
+SRC_PATH="../../src"
+DOJO_PATH="../../src/dojo"
+
+cat\
+    $SRC_PATH/dojo.js \
+    $DOJO_PATH/array.js \
+    $DOJO_PATH/connect.js \
+    > ../../dist/dojo-webkit-mobile-kitchensink.js
