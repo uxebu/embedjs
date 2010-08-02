f411068255d997db2f7cf2396bbdb7f00ae173c6
Jens Arps <arps@uxebu.com>
2010-08-02 15:26:18 +0200

    Seperating connect/event, adding pub/sub

--- src/dojo/event.js
+++ src/dojo/event.js
@@ -1,10 +1,78 @@
-dojo.stopEvent = function(/*Event*/ evt){
-	// summary:
-	//		prevents propagation and clobbers the default action of the
-	//		passed event
-	// evt: Event
-	//		The event object. If omitted, window.event is used on IE.
-	evt.preventDefault();
-	evt.stopPropagation();
-	// NOTE: below, this method is overridden for IE
-};
+(function(){
+
+var del = (dojo._event_listener = {
+		add: function(/*DOMNode*/ node, /*String*/ name, /*Function*/ fp){
+			if(!node){return;} 
+			name = del._normalizeEventName(name);
+			node.addEventListener(name, fp, false);
+			return fp; /*Handle*/
+		},
+		remove: function(/*DOMNode*/ node, /*String*/ event, /*Handle*/ handle){
+			// summary:
+			//		clobbers the listener from the node
+			// node:
+			//		DOM node to attach the event to
+			// event:
+			//		the name of the handler to remove the function from
+			// handle:
+			//		the handle returned from add
+			if(node){
+				event = del._normalizeEventName(event);
+				node.removeEventListener(event, handle, false);
+			}
+		},
+		_normalizeEventName: function(/*String*/ name){
+			// Generally, name should be lower case, unless it is special
+			// somehow (e.g. a Mozilla DOM event).
+			// Remove 'on'.
+			return name.slice(0,2) =="on" ? name.slice(2) : name;
+		}
+	});
+
+	// DOM events
+	
+	dojo.fixEvent = function(/*Event*/ evt, /*DOMNode*/ sender){
+		// summary:
+		//		normalizes properties on the event object including event
+		//		bubbling methods, keystroke normalization, and x/y positions
+		// evt: Event
+		//		native event object
+		// sender: DOMNode
+		//		node to treat as "currentTarget"
+		return del._fixEvent(evt, sender);
+	};
+
+	dojo.stopEvent = function(/*Event*/ evt){
+		// summary:
+		//		prevents propagation and clobbers the default action of the
+		//		passed event
+		// evt: Event
+		//		The event object. If omitted, window.event is used on IE.
+		evt.preventDefault();
+		evt.stopPropagation();
+		// NOTE: below, this method is overridden for IE
+	};
+	
+	// Unify connect and event listeners
+	dojo._connect = function(obj, event, context, method, dontFix){
+		// FIXME: need a more strict test
+		var isNode = obj && (obj.nodeType||obj.attachEvent||obj.addEventListener);
+		// choose one of three listener options: raw (connect.js), DOM event on a Node, custom event on a Node
+		// we need the third option to provide leak prevention on broken browsers (IE)
+		var lid = isNode ? 1 : 0, l = [dojo._listener, del][lid];
+		// create a listener
+		var h = l.add(obj, event, dojo.hitch(context, method));
+		// formerly, the disconnect package contained "l" directly, but if client code
+		// leaks the disconnect package (by connecting it to a node), referencing "l" 
+		// compounds the problem.
+		// instead we return a listener id, which requires custom _disconnect below.
+		// return disconnect package
+		return [ obj, event, h, lid ];
+	};
+
+	dojo._disconnect = function(obj, event, handle, listener){
+		([dojo._listener, del][listener]).remove(obj, event, handle);
+	};
+
+	
+})();
