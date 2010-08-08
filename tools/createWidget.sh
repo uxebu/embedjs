be508088b906f49d0e9659fe25a8d01845e4877d
Wolfram Kriesing <wk@uxebu.com>
2010-08-08 23:02:16 +0200

    some renaming and added nokia wrt as platform

--- tools/createWidget.sh
+++ tools/createWidget.sh
@@ -16,12 +16,16 @@ find . -type f -name "*.uncompressed.js" | xargs rm
 rm ../embedJStests.wgt
 zip -r ../embedJStests.wgt *
 
+
+
 # Create the Nokia WRT widget.
 rm config.xml
 cp ../../src/Info.plist .
 cd ..
 mv wgt src # A nokia WRT widget has to be in the directory "src" for packaging it ... for whatever reason, but it wont install otherwise ... grrrr
 zip -r embedJStests.wgz src/*
+
+
 
 # Clean up
 rm -Rf src
