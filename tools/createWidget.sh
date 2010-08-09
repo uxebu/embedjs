9ae529d7a7c06ce36231df69da38eb809cd59f22
Wolfram Kriesing <wk@uxebu.com>
2010-08-09 21:45:34 +0200

    Optimize the Nokia WRT widget size a bit.

--- tools/createWidget.sh
+++ tools/createWidget.sh
@@ -3,12 +3,13 @@ cd ..
 # Empty the widget stuff
 rm -Rf tmp/wgt
 # Create the temp dir where we are going to build the widget in
-mkdir -p tmp/wgt
+# Create a "embedJS" directory so we can remove all but nokia later for the Nokia WRT, to reduce the size.
+mkdir -p tmp/wgt/embedJS
 
 cd tmp/wgt
 cp -R ../../src/tests/* .
 cp ../../src/config.xml .
-cp ../../build/embed-* .
+cp ../../build/embed-* ./embedJS
 
 # Remove all *.uncompressed.js files, they are just bloat in a widget
 find . -type f -name "*.uncompressed.js" | xargs rm
@@ -19,10 +20,14 @@ zip -r ../embedJStests.wgt *
 
 
 # Create the Nokia WRT widget.
+echo
+echo "*** Create Nokia WRT widget ***"
+echo 
 rm config.xml
 cp ../../src/Info.plist .
 cd ..
 mv wgt src # A nokia WRT widget has to be in the directory "src" for packaging it ... for whatever reason, but it wont install otherwise ... grrrr
+find src/embedJS -type f ! -name "*nokia*" -delete
 zip -r embedJStests.wgz src/*
 
 
