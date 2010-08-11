281593891de693f46452c51329f76732a71e1d84
Wolfram Kriesing <wk@uxebu.com>
2010-08-11 22:52:28 +0200

    Create the index.html for all platforms found.

--- tools/createRunTests.js
+++ tools/createRunTests.js
@@ -12,12 +12,33 @@ function getPlatforms(dir){
 	return platforms;
 }
 
-function renderTpl(content, platform, isWidget){
+function renderRunTestsTpl(content, platform, isWidget){
+	// summary: Render the runTests.html.tpl for the given platform.
 	var filename = isWidget ? "embedJS/" : "../../build/";
 	filename += "embed-kitchensink-" + platform + ".js"
 	var ret = content.replace("${embedjs_filename}", filename);
 	var ret = ret.replace("${platform}", platform);
 	return ret;
+}
+
+function renderIndexTpl(platforms){
+	// summary: Take the index.html.tpl apart and render the {loop} part in there for all platforms.
+	var tpl = readFile(RUN_TESTS_DIR+"/index.html.tpl");
+	// Parse out the ONE loop that is in the index.html.tpl
+	var loop = tpl.match(/([.\s\S]*)\{loop\}([.\s\S]*)\{endloop\}([.\s\S]*)/);
+	var loopContent = loop[2];
+	var ret = "";
+	for (var i=0, l=platforms.length, p; i<l; i++){
+		p = platforms[i];
+		ret += 	loopContent.replace(/\$\{platform\}/g, p);
+	}
+	return loop[1] + ret + loop[3];
+}
+
+function writeFile(fileName, content){
+	var f = new FileWriter(fileName);
+	f.write(content);
+	f.close();
 }
 
 //
@@ -31,13 +52,12 @@ for (var i=0, l=platforms.length, p; i<l; i++){
 	// Write the normal file.
 	var destFile = RUN_TESTS_DIR+"/runTests-" + p + ".html";
 	print("Writing '" + destFile + "'");
-	var f = new FileWriter(destFile);
-	f.write(renderTpl(tpl, p));
-	f.close();
+	writeFile(destFile, renderRunTestsTpl(tpl, p));
 	// Write the test file for the widget env.
 	var destFile = RUN_TESTS_DIR+"/runTests-widget-" + p + ".html";
 	print("Writing '" + destFile + "'");
-	var f = new FileWriter(destFile);
-	f.write(renderTpl(tpl, p, true));
-	f.close();
+	writeFile(destFile, renderRunTestsTpl(tpl, p, true));
 }
+destFile = RUN_TESTS_DIR+"/index.html";
+print("Writing '" + destFile + "'");
+writeFile(destFile, renderIndexTpl(platforms));
