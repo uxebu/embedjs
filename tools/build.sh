4296321eeeb7c30ef53e958dcddec390c55e6104
Wolfram Kriesing <wk@uxebu.com>
2010-08-12 23:27:14 +0200

    still playing with it :)

--- tools/build.sh
+++ tools/build.sh
@@ -1,42 +1,37 @@
-cd .. # Lets change to the root directory
+DIR=`dirname $0`
 
 # TODO actually the check should valllidate if this is a valid definition file
 if [ -z "$1" ]; then
 	echo "Please specify the kind of build you would like, choose from"
 	# TODO the follwoing should actually ONLY be a list of the valid 
-	cd profiles
-	find . -type f -name "*.definition" | xargs basename -a -s .definition
+	find $DIR/../profiles -type f -name "*.definition" | xargs basename -a -s .definition
 	echo 
 	exit;
 fi
 
-PLATFORMS=`find profiles/platforms -type f -name "*.json"`
-#PLATFORMS=profiles/platforms/iPhone.json
-cd tools
+PLATFORMS=`find $DIR/../profiles/platforms -type f -name "*.json"`
+echo $PLATFORMS
 for P in $PLATFORMS; do
 
 	# The following has to be executed in "tools" directory, since getFiles.js relys on the path.
-	cd ../tools
-	FILES=$(java -jar js.jar getFiles.js "../$P" `cat ../profiles/$1.definition`)
+	FILES=$(java -jar $DIR/js.jar $DIR/getFiles.js "$DIR/../$P" $DIR/../src/dojo `cat $DIR/../profiles/$1.definition`)
+	echo $FILES
 	
-	cd ..
-	PLATFORM_NAME=`basename "$P" .json`
-	if [ "${FILES:0:6}" = "ERROR:" ]; then
-		echo $FILES
-		exit;
-	fi
-	cd src
-	DEST_FILE=../build/embed-$1-$PLATFORM_NAME.js
-	DEST_FILE_UNCOMPRESSED=../build/embed-$1-$PLATFORM_NAME.uncompressed.js
-	#echo $FILES
-	java -jar ../tools/shrinksafe.jar $FILES > $DEST_FILE
-	echo "created `du -h $DEST_FILE`"
-	# Create uncompressed files
-	cat $FILES > $DEST_FILE_UNCOMPRESSED
-	echo "created `du -h $DEST_FILE_UNCOMPRESSED`"
+	#PLATFORM_NAME=`basename "$P" .json`
+	#if [ "${FILES:0:6}" = "ERROR:" ]; then
+	#	echo $FILES
+	#	exit;
+	#fi
+	#DEST_FILE=$DIR/../build/embed-$1-$PLATFORM_NAME.js
+	#DEST_FILE_UNCOMPRESSED=$DIR/../build/embed-$1-$PLATFORM_NAME.uncompressed.js
+	#cd $DIR/../src
+	#java -jar $DIR/shrinksafe.jar $FILES > $DEST_FILE
+	#echo "created `du -h $DEST_FILE`"
+	## Create uncompressed files
+	#cat $FILES > $DEST_FILE_UNCOMPRESSED
+	#echo "created `du -h $DEST_FILE_UNCOMPRESSED`"
 done
 
-echo
-echo "Creating runTests-*.html files"
-cd ../tools
-./createRunTests.sh
+#echo
+#echo "Creating files in src/tests"
+#$DIR/createRunTests.sh
