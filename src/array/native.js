75bddd53d76ba5c1737ab3711f0984270e876fd7
Jens Arps <arps@uxebu.com>
2011-03-03 23:31:19 +0100

    Comments on the lastIndexOf issue.

--- src/array/native.js
+++ src/array/native.js
@@ -1,8 +1,10 @@
 ["indexOf", "lastIndexOf"].forEach(
 	function(name, idx){
 		dojo[name] = function(arr, callback, thisObj){
-			// This is due to a bug in Chrome 9, FF 3.6 and 4
-			// Do we want to have this in an own feature?
+			// This is due to a bug found in in Chrome 9, FF 3.6 and 4:
+			// Having undefined as the second parameter to lastIndexOf
+			// will result in lastIndeOf interpreting this as 0.
+			// TODO: Do we want to have this in an own feature?
 			return typeof thisObj == "undefined" ? 
 				Array.prototype[name].call(arr, callback) : 
 				Array.prototype[name].call(arr, callback, thisObj);
