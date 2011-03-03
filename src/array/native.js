60d995f8202a97d809cd85116b14d6822817d43c
Jens Arps <arps@uxebu.com>
2011-03-03 21:28:21 +0100

    Working around a bug in recent FF and Chrome browsers

--- src/array/native.js
+++ src/array/native.js
@@ -1,7 +1,18 @@
-["indexOf", "lastIndexOf", "forEach", "map", "some", "every", "filter"].forEach(
+["indexOf", "lastIndexOf"].forEach(
 	function(name, idx){
 		dojo[name] = function(arr, callback, thisObj){
-			if((idx > 1) && (typeof callback == "string")){
+			// This is due to a bug in Chrome 9, FF 3.6 and 4
+			// Do we want to have this in an own feature?
+			return typeof thisObj == "undefined" ? 
+				Array.prototype[name].call(arr, callback) : 
+				Array.prototype[name].call(arr, callback, thisObj);
+		}
+	}
+);
+["forEach", "map", "some", "every", "filter"].forEach(
+	function(name, idx){
+		dojo[name] = function(arr, callback, thisObj){
+			if(typeof callback == "string"){
 				callback = new Function("item", "index", "array", callback);
 			}
 			return Array.prototype[name].call(arr, callback, thisObj);
