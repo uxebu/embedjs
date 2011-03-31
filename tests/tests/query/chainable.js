a04fb564829a0b08e169a00f8a9011205b7f4123
Wolfram Kriesing <wk@uxebu.com>
2011-03-31 16:45:21 +0200

    Added quite some tests, esp. to test chaining multiple times, etc.

--- tests/tests/query/chainable.js
+++ tests/tests/query/chainable.js
@@ -35,28 +35,78 @@ tests.register("query-chainable",
 		},
 		
 		function callStyleFunction(){
-			var res = embed.query("#_foo");
-			res.style("color", "red")
-			doh.is("red", res.style("color"));
+			var n = embed.query("#_foo")
+			n.style("color", "red");
+			doh.is("red", embed.style(n[0], "color"));
+		},
+		
+		function callStyleFunctionMultipleNodes(){
+			var n = embed.query("#_foo, .foo, .subDiv")
+			n.style("color", "red");
+			for (var i=0, l=n.length; i<l; i++){
+				doh.is("red", embed.style(n[i], "color"));
+			}
 		},
 		
 		function callAttrFunction(){
-			var res = embed.query("#_foo");
-			res.attr("data-x", "f");
-			doh.is("f", res.attr("data-x"));
+			var n = embed.query("#_foo");
+			n.attr("data-x", "f");
+			doh.is("f", embed.attr(n[0], "data-x"));
+		},
+		
+		function callAttrFunctionMultipleNodes(){
+			var n = embed.query("#_foo, .foo, .subDiv")
+			n.attr("data-x", "f");
+			for (var i=0, l=n.length; i<l; i++){
+				doh.is("f", embed.attr(n[i], "data-x"));
+			}
 		},
 		
 		function callAddClassFunction(){
-			var res = embed.query("#_foo");
-			res.addClass("f");
-			doh.assertTrue(embed.hasClass(embed.query("#_foo")[0], "f"));
+			var n = embed.query("#_foo");
+			n.addClass("f");
+			doh.assertTrue(embed.hasClass(n[0], "f"));
 		},
 		
 		function callRemoveClassFunction(){
-			var res = embed.query("#_foo");
-			res.addClass("f");
-			res.removeClass("f");
-			doh.assertFalse(embed.hasClass(embed.query("#_foo")[0], "f"));
+			var n = embed.query("#_foo");
+			n.addClass("f");
+			n.removeClass("f");
+			doh.assertFalse(embed.hasClass(n[0], "f"));
+		},
+		
+		function callMultipleFunctions(){
+			var n = embed.query("#_foo");
+			n.addClass("f")
+			 .attr("data-x", "whateva")
+			 .style("color", "blue");
+			doh.assertTrue(embed.hasClass(n[0], "f"));
+			doh.assertEqual("whateva", embed.attr(n[0], "data-x"));
+			doh.assertEqual("blue", embed.style(n[0], "color"));
+		},
+		
+		function callMultipleFunctions1(){
+			var n = embed.query("#_foo");
+			n.attr("data-x", "y")
+			 .addClass("z")
+			 .connect("onclick", console.log) // TODO how can we test this? missing assert for this still
+			 .removeAttr("data-x")
+			 .removeClass("z")
+			 .style("color", "white")
+			 .toggleClass("u");
+			 //.place(); too lazy to look up how it works :)
+			doh.assertTrue(embed.hasClass(n[0], "u"));
+			doh.assertFalse(embed.hasClass(n[0], "z"));
+			doh.assertEqual(null, embed.attr(n[0], "data-x"));
+			doh.assertEqual("white", embed.style(n[0], "color"));
+		},
+		
+		//
+		// Use array functions in the chaining
+		//
+		function callArrayMap(){
+			embed.query("#_foo").filter(console.log);
+			//doh.assertFalse(embed.hasClass(embed.query("#_foo")[0], "f"));
 		}
 	]
 );
