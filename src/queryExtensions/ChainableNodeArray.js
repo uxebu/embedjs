87cdd0d38807c3cf034cb900924bd70fc139a9cf
David Aurelio <david@uxebu.com>
2011-04-01 12:31:22 +0200

    Fixing conversion of `arguments` to array.
    - replacing `splice` by `slice`
    - creating local reference to `Array.prototype.slice` to avoid object creation or lookups later

--- src/queryExtensions/ChainableNodeArray.js
+++ src/queryExtensions/ChainableNodeArray.js
@@ -1,29 +1,31 @@
 ;(function(){
+	var slice = [].slice;
+
 	// Remember the old query function, so we can still call it.
 	var _oldQuery = embed.query;
 	// Override embed.query() with a chainable version of itself.
 	embed.query = function(query, scope){
 		return new embed.ChainableNodeArray(_oldQuery.apply(embed, arguments));
 	};
-	
+
 	// Extend the Array prototype for the ChainableNodeArray to provide all methods that
 	// are reachable by chainable functions.
 	embed.ChainableNodeArray = function(arr){
 		//var ret = Array.apply(null, arr);
 		// "arr" is a NodeList object and WebKit is not able to use that as parameters to push(), ff can though.
 		// So let's explicitly convert the NodeList into an array.
-		var ret = Array.prototype.slice.call(arr); // or [].slice.call(arr) for less bytes
+		var ret = slice.call(arr);
 		makeChainable(ret);
 		return ret;
 	};
-	
+
 	function makeChainable(obj){
 		// add chained functions
 		embed.forEach(
 			["attr", "addClass", "connect", "removeAttr", "removeClass", "style", "toggleClass", "place"],
 			function(func){
 				this[func] = function(){
-					var argsAsArray = [].splice.call(arguments); // Convert arguments into an array, so we can use cancat() on it.
+					var argsAsArray = slice.call(arguments); // Convert arguments into an array, so we can use cancat() on it.
 					for (var i=0, l=this.length; i<l; i++){
 						// Concatenate this[i]+arguments into one array to be able to pass them as ONE array.
 						// "this[i]" is the current node, since this is the array we are in, the array with all the nodes query() returned.
@@ -41,7 +43,7 @@
 			["forEach", "map", "some", "every", "filter"],
 			function(func){
 				this[func] = function(){
-					var argsAsArray = [].splice.call(arguments,0); // Convert arguments into an array, so we can use cancat() on it.
+					var argsAsArray = slice.call(arguments,0); // Convert arguments into an array, so we can use cancat() on it.
 					var ret = embed[func].apply(embed, [this].concat(argsAsArray));
 					// The result we get returned above is a native array, let's convert
 					// it into a chainable one again so the chaining can go on.
@@ -53,4 +55,3 @@
 		);
 	}
 })();
-
