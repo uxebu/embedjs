806bc4932eca6ab9ed56f1143fc2bf850e224b4d
Jens Arps <arps@uxebu.com>
2011-08-05 16:36:07 +0200

    Added scope param to query:simple

--- src/query/simple.js
+++ src/query/simple.js
@@ -1,15 +1,23 @@
-define(['embed'], function(embed){
+define(['embed', 'feature!html-id'], function(embed){
 
-	embed.query = function(query, parentNode){
+	embed.query = function(query, scope){
 		// summary: Works with ".className", "#id", and ".className .className"
-		// Actually we would need querySelectorAll() :-).
-		// But we don't have it, so we just kinda fake it.
+		
+		// scope normalization
+		if(typeof scope == "string"){
+			scope = embed.byId(scope);
+			if(!scope){
+				return [];
+			}
+		}
+		scope = scope || embed.doc;
+		
 		var parts = query.split(" ");
 		if (parts.length > 1){
-			return this.query(parts.slice(1).join(" "), this.query(parts[0]));
+			return embed.query(parts.slice(1).join(" "), embed.query(parts[0]));
 		}
 		if (query.charAt(0)=="."){
-			return document.getElementsByClassName(query.substr(1));
+			return scope.getElementsByClassName(query.substr(1));
 		}
 		if (query.charAt(0)=="#"){
 			return [document.getElementById(query.substr(1))];
