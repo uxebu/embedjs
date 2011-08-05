14d4397ae04a48ecd9c9449b2e387f0e50959b8c
Jens Arps <arps@uxebu.com>
2011-08-05 11:01:45 +0200

    Added stripComments param to fromJson (for compat reasons; dojo's fromJson doesn't throw on comments inside JSON)

--- src/json/json.js
+++ src/json/json.js
@@ -3,11 +3,16 @@ define(['embed'], function(embed){
 	// NOTE: dojo's JSON impl differs from native!
 	//	(e.g. revier function)
 	
+	var commentRegExp = /(\/\*([\s\S]*?)\*\/|\/\/(.*)$)/mg;
+	
 	embed.toJson = function(/* Mixed */ data){
 		return JSON.stringify(data);
 	};
 	
-	embed.fromJson = function(/* String */ json){
+	embed.fromJson = function(/* String */ json, /* Boolean? */ stripComments){
+		if(stripComments){
+			json = json.replace(commentRegExp, '');
+		}
 		return JSON.parse(json);
 	}
 	
