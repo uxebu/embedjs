433ef989a18a8c1187dee5be3987ed99dde13c6f
Jens Arps <arps@uxebu.com>
2011-08-05 16:07:59 +0200

    Ported query:simple

--- src/query/simple.js
+++ src/query/simple.js
@@ -1,17 +1,22 @@
+define(['embed'], function(embed){
 
-dojo.query = function(query, parentNode){
-	// summary: Works with ".className", "#id", and ".className .className"
-	// Actually we would need querySelectorAll() :-).
-	// But we don't have it, so we just kinda fake it.
-	var parts = query.split(" ");
-	if (parts.length > 1){
-		return this.query(parts.slice(1).join(" "), this.query(parts[0]));
-	}
-	if (query.charAt(0)=="."){
-		return document.getElementsByClassName(query.substr(1));
-	}
-	if (query.charAt(0)=="#"){
-		return [document.getElementById(query.substr(1))];
-	}
-	return [];
-};
+	embed.query = function(query, parentNode){
+		// summary: Works with ".className", "#id", and ".className .className"
+		// Actually we would need querySelectorAll() :-).
+		// But we don't have it, so we just kinda fake it.
+		var parts = query.split(" ");
+		if (parts.length > 1){
+			return this.query(parts.slice(1).join(" "), this.query(parts[0]));
+		}
+		if (query.charAt(0)=="."){
+			return document.getElementsByClassName(query.substr(1));
+		}
+		if (query.charAt(0)=="#"){
+			return [document.getElementById(query.substr(1))];
+		}
+		return [];
+	};
+
+	return embed;
+
+});
