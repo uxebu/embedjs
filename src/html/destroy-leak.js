995a5957d4349daca06c026f4610c3ae2cf6dc6c
Jens Arps <arps@uxebu.com>
2011-08-05 10:32:55 +0200

    Cleaned up destroy-leak

--- src/html/destroy-leak.js
+++ src/html/destroy-leak.js
@@ -1,10 +1,12 @@
-//require.modify("dojo/destroy", ["dojo"], function(){
-// extension to prevent memory leaks
-// load it in conjunction with destroy/_base.js
+define(['embed', 'feature!html-destroy'], function(embed){
 
-// find a way to access to destroyContainer defined in destroy/_base.js
+	// find a way to access to destroyContainer
+	// need to add embed.addOnWindowUnload
 
-//dojo.addOnWindowUnload(function(){
-//	_destroyContainer = null; //prevent IE leak
-//});
-//});
+//	embed.addOnWindowUnload(function(){
+//		_destroyContainer = null; //prevent IE leak
+//	};
+
+	return embed;
+
+});
