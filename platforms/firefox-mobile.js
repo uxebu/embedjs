913bc1a462d5155270da680a5e24ec855e14b611
Jens Arps <arps@uxebu.com>
2011-08-15 15:20:39 +0200

    Added some implementation maps.

---
+++ platforms/firefox-mobile.js
@@ -0,0 +1,94 @@
+// Implementation map for Firefox mobile 5+
+define({
+	'embed': 'embed',
+	
+	// Feature 'array'
+	
+	'array': 'array/native',
+	
+	// Feature group 'connect'
+	
+	'connect-connect': 'connect/connect',
+	
+	'connect-event': 'connect/event',
+	
+	'connect-pubsub': 'connect/pubsub',
+	
+	// Feature group 'async'
+	
+	'async-promise': 'async/promise',
+	
+	'async-when': 'async/when',
+	
+	'async-deferred': 'async/deferred',
+	
+	// Feature 'geolocation' || TODO: Shoudn't this be sth like deviceAPIs?
+	
+	// 'geolocation': 'geolocation/w3c', // Removed feature. We don't offer anything here but a memory pointer.
+	
+	// Feature group 'html'
+	
+	'html-attr': 'html/attr',
+	
+	'html-element': 'html/element',
+	
+	'html-todom': 'html/todom',
+	
+	'html-class': 'html/class-classlist',
+	
+	'html-style': 'html/style',
+	
+	'html-ready': 'html/ready',
+	
+	'html-id': 'html/id',
+	
+	'html-destroy': 'html/destroy',
+	
+	// Feature 'json' || TODO: put this in some group?
+	
+	'json':  'json/native',
+	
+	// Feature group 'lang'
+	
+	'lang-toarray': 'lang/toarray',
+	
+	'lang-clone': 'lang/clone',
+	
+	'lang-hitch': 'lang/hitch',
+	
+	'lang-is': 'lang/is',
+	
+	'lang-string': 'lang/string',
+	
+	'lang-mixin': 'lang/mixin',
+	
+	'lang-object': 'lang/object',
+	
+	// Feature group 'transport'
+	
+	'transport-xhr': 'transport/xhr',
+	
+	'transport-script': 'transport/script',
+	
+	'transport-jsonp': 'transport/jsonp',
+	
+	// Feature 'query'
+	
+	'query': 'query/qsa-preprocessor',
+	
+	// Feature group 'queryExtensions'
+	
+	'queryExtensions-ChainableNodeArray': 'queryExtensions/ChainableNodeArray',
+	
+	// Feature group 'oo'
+	
+	'oo-declare': 'oo/declare',
+	
+	'oo-delegate': 'oo/delegate',
+	
+	'oo-extend': 'oo/extend',
+	
+	// Feature group 'uri'
+	
+	'uri': 'uri/objectToQuery'
+});
