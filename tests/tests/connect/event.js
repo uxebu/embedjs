29a7f51c8fa10b79854fe39aba1a08bc37e55f72
Jens Arps <arps@uxebu.com>
2011-10-06 12:11:21 +0200

    Starting tests for event.

--- tests/tests/connect/event.js
+++ tests/tests/connect/event.js
@@ -1,7 +1,24 @@
-tests.register("connect-event",
-	[
-	 	function _start(){
-	 		console.warn('No Tests for connect-event!');
-	 	}
-	]
-);
+require(['text!../tests/tests/connect/event.html'], function(html){
+
+	document.body.innerHTML = html;
+	
+	function simulateClick(node) {
+		var evt = document.createEvent("MouseEvents");
+		evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		console.log('dispatching event:', evt, node);
+		return node.dispatchEvent(evt);
+	}
+	
+	tests.register("connect-event",
+		[
+		 	function simpleConnect(){
+		 		var node = document.getElementById('inner');
+		 		embed.connect(node, 'onclick', function(){
+		 			console.log('clicked!');
+		 		});
+		 		simulateClick(node);
+		 	}
+		]
+	);
+
+});
