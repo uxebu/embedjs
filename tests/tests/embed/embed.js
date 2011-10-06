f9b58a52429d45612f12826587195bb3aa6e5fd1
Jens Arps <arps@uxebu.com>
2011-10-06 11:33:35 +0200

    Added tests for embed core.

--- tests/tests/embed/embed.js
+++ tests/tests/embed/embed.js
@@ -1,7 +1,23 @@
 tests.register("embed",
-	[
-	 	function NoTestsAvailable(){
-	 		console.warn('No Tests for embed');
+	[	
+	 	function documentRef(t){
+	 		t.is(document, embed.doc);
+	 	},
+	 	
+	 	function globalRef(t){
+	 		t.is(window, embed.global);
+	 	},
+	 	
+	 	function body(t){
+	 		t.is(document.body, embed.body());
+	 	},
+	 	
+	 	function configObject(t){
+	 		t.is('[object Object]', {}.toString.call(embed.config));
+	 	},
+	 	
+	 	function versionString(t){
+	 		t.is('[object String]', {}.toString.call(embed.version));
 	 	}
 	]
 );
