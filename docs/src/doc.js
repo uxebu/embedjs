0f9cc108fe1c5854fb63de8a1c3bd4500c575e72
Jens Arps <arps@uxebu.com>
2011-10-10 13:25:33 +0200

    Building a simple live docs generator

---
+++ docs/src/doc.js
@@ -0,0 +1,89 @@
+require(['embed', '../docs/src/parser', '../profiles/kitchensink'], function(embed, parser){
+	
+	var docTool = {
+			
+		data: [],
+		
+		domNode: null,
+		
+		itemTemplates: {
+			'tpl-function': '<h1><a name="{name}">{name}</a></h1><h2>{type}</h2><div>{detailsTable}</div>',
+			'tpl-default': '<h1><a name="{name}">{name}</a></h1><h2>{type}</h2>',
+			'tpl-doc-item': '<tr><td>{name}</td><td>{desc}</td></tr>',
+			'tpl-doc-param': '<tr><td>{name}</td><td>{type}</td><td>{optional}</td><td>{desc}</td></tr>'
+		},
+			
+		init: function(){
+			this.domNode = embed.body();
+			this.data = parser.run(embed);
+			
+			this.display();
+		},
+		
+		display: function(){
+			embed.forEach(this.data, function(item){
+				if(item.isPrivate){
+					return;
+				}
+				
+				if(item.type == 'function'){
+					item.name += '(' + embed.map(item.params, function(_item){ return _item.name; }).join(', ') + ')';
+					
+					var paramDescs = {};
+					var detailsTable = '<table>';
+					for(var section in item.doc){
+						var desc = "";
+						var isCode = false;
+						embed.forEach(item.doc[section], function(_line){
+							if(_line.substring(0,1) == '|'){
+								if(!isCode){
+									desc += '<pre>';
+									isCode = true;
+								}
+								desc += _line+ '\n';
+							}else{
+								if(isCode){
+									desc += '</pre>';
+									desc += _line + '\n';
+									isCode = false;
+								}else{
+									desc += _line + '\n';
+								}
+							}
+						}, this);
+						item.doc[section] = desc;
+						
+						
+						if(embed.indexOf(parser.keywords, section) != -1){
+							detailsTable += embed.replace(this.itemTemplates['tpl-doc-item'], { name: section, desc: item.doc[section] });
+						}else{
+							paramDescs[section] = item.doc[section];
+						}
+					}
+					if(item.params.length){
+						detailsTable += '<tr><td>Parameters</td><td><table>';
+						detailsTable += '<thead><tr><td>Name</td><td>Type</td><td>Optional</td><td>Description</td></tr></thead><tbody>'
+						embed.forEach(item.params, function(_param){
+							_param.desc = paramDescs[_param.name] || '';
+							detailsTable += embed.replace(this.itemTemplates['tpl-doc-param'], _param);
+						}, this);
+						detailsTable += '</tbody></table></td></tr>';
+					}
+					detailsTable += '</table>';
+					
+					item.detailsTable = detailsTable;
+				}
+				
+				embed.create('div', {
+					className: 'item',
+					innerHTML: embed.replace(this.itemTemplates['tpl-' + item.type] || this.itemTemplates['tpl-default'], item)
+				}, this.domNode);
+			}, this);
+		}
+		
+	};
+	
+	docTool.init();
+	window.docTool = docTool;
+
+});
---
+++ docs/src/parser.js
@@ -0,0 +1,130 @@
+define(['embed'], function(embed){
+	
+	var parser = {
+		
+		data: [],
+		
+		keywords: ['summary', 'description', 'example', 'returns', 'FIXME', 'TODO'],
+		
+		run: function(toParse){
+			this.crawl(toParse);
+			this.sort();
+			return this.data;
+		},
+		
+		crawl: function(obj){
+		
+			for(var propName in obj){
+				
+				var item = {};
+				
+				item.name = propName;
+				
+				item.isPrivate = propName.substring(0, 1) == '_';
+				
+				switch(true){
+					case embed.isFunction(obj[propName]):
+						item.type = propName.substring(0, 1).toLowerCase() == propName.substring(0, 1) ? 'function' : 'constructor';
+						item.source = obj[propName].toString();
+						this.extractComments(item);
+						this.extractParams(item);
+						this.parseComments(item);
+						break;
+					case embed.isObject(obj[propName]):
+						item.type = 'object';
+						break;
+					case embed.isString(obj[propName]):
+						item.type = 'string';
+						break;
+					default:
+						item.type = typeof obj[propName];
+				}
+				
+				this.data.push(item);
+			}
+		},
+		
+		sort: function(){
+			this.data.sort(function(a, b){
+				return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+			});
+		},
+		
+		extractComments: function(item){
+			var lines = item.source.split('\n');
+			var comments = [];
+			embed.some(lines, function(line, index){
+				if(index == 0){
+					// skip first line
+					return false;
+				}
+				line = embed.trim(line);
+				if(line.indexOf('//') == 0){
+					comments.push(line.substring(2));
+					return false;
+				}else{
+					return true; // end parsing.
+				}
+			}, this);
+			item.comments = comments;
+		},
+		
+		extractParams: function(item){
+			var params = [];
+			var header = item.source.split('\n')[0];
+			var paramString = header.substring(header.indexOf('(') + 1, header.indexOf(')'));
+			if(paramString.length){
+				var paramItems = paramString.split(',');
+				embed.forEach(paramItems, function(item){
+					var param, hint, optional;
+					item = embed.trim(item);
+					var hintParts = item.split('*/');
+					if(hintParts.length > 1){
+						param = embed.trim(hintParts[1]);
+						hint = hintParts[0].substring(2, hintParts[0].length);
+						if(hint.indexOf('?') != -1){
+							hint = hint.substring(0, hint.length - 1);
+							optional = true;
+						}else{
+							optional = false;
+						}
+					}else{
+						param = item;
+						hint = '';
+						optional = false;
+					}
+					params.push({ name: param, type: hint, optional: optional });
+				}, this);
+			}
+			item.params = params;
+		},
+		
+		parseComments: function(item){
+			var doc = {};
+			var section;
+			var keywords = this.keywords.concat(embed.map(item.params, function(_item){ return _item.name; }));
+			embed.forEach(item.comments, function(line){
+				line = embed.trim(line);
+				// If that line starts with a keyword and a ':',
+				// it's likely a section.
+				var _parts = line.split(':');
+				if(_parts.length == 2 && embed.indexOf(keywords, _parts[0]) != -1){
+					// section start
+					section = _parts[0];
+					doc[section] = [];
+				}else{
+					// section continues
+					section && doc[section].push(line);
+				}
+			}, this);
+			
+//			for(section in doc){
+//				doc[section] = doc[section].join('\n');
+//			}
+			item.doc = doc;
+		}
+	};
+	
+	return parser;
+
+});
