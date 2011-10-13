0dcf5f0a23f0de9e3c0e2f2804040788a04ef7d5
Jens Arps <arps@uxebu.com>
2011-10-13 10:37:44 +0200

    Seperated making embed object global, pt 2

--- profiles/oo.js
+++ profiles/oo.js
@@ -4,13 +4,9 @@
 define([
 	"feature!oo-declare",
 	"feature!oo-delegate",
-	"feature!oo-extend"
+	"feature!oo-extend",
+	"feature!global"
 ],
 function(embed){
-	// This is the place to attach
-	// embed to the global namespace,
-	// if we want to.
-	embed.global.embed = embed;
-	
 	return embed;
 });
